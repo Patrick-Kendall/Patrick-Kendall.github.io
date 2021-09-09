d41668aa17512390fd7bfc7cce710253c334cf85
Patrick Kendall <pkendall147@gmail.com>
2021-09-09 16:38:12 -0400

    remove Social Contract styling; add getFeatured.js and AJAX functionality
    index.html contains a script which fetches a review with class "featured" and displays on index.html
    
    remove table
    
    fix "first-paragraph" ; standardize review headings

---
+++ js/getFeatured.js
@@ -0,0 +1,27 @@
+
+document.addEventListener('onscroll', loadFeatured());
+
+function loadFeatured() {
+  var lit = new XMLHttpRequest();
+
+   lit.open('GET','poly.html',true);
+   lit.responseType = 'document';
+
+   // let page = this.response;
+   lit.onload = function(){
+     if(this.status == 200){
+       const page = this.response;
+       console.log(page);
+
+       document.querySelector('.review').innerHTML = page.querySelector('.featured').innerHTML;
+     }
+   }
+   // document.querySelector('.review').innerHTML = page.querySelector('.featured');
+
+   // console.log(lit.querySelector('.featured'));
+
+
+   // console.log(this.response);
+   lit.send();
+
+}
